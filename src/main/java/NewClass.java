c88dd816f25f6da7858deafc5c17ab45b4048f5f
Juan Felipe Carvajal <j.felipe_16@hotmail.com>
2018-09-11 20:53:33 -0500

    inicial

---
+++ src/main/java/NewClass.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Estudiante
+ */
+public class NewClass {
+    public static void main(String[] args) {
+        int x;
+        x = 2+3;
+        System.out.println("Hola mundo "+x);
+    }
+    
+}
